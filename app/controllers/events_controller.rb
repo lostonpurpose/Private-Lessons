b2926c1082c026300032a0d1e22f6255bcb4cf6b
Arisa Segawa <arisa.segawa@gmail.com>
2024-11-26 17:32:22 +0900

    done the index, new, create actions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,29 @@
 class EventsController < ApplicationController
 
   def index
-    @events = Event.all
+    @user = current_user
+    @events = @user.events
+    @event_bookings = Booking.where(events_id: @events)
+    @bookings = @user.bookings
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.user = current_user
+      if @event.save
+        redirect_to root_path #need to change this to dashboard
+      else
+        render 'new', status: :unprocessable_entity
+      end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :capacity, :price, :location_id, :start_date, :end_date)
   end
 end
