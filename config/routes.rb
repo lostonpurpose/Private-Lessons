21f973db03e6c0d3af1ee7357ff167f16acead58
Will VanderWyden <vanderwyden@gmail.com>
2024-11-26 13:13:19 +0900

    made the routes :|

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,18 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resources :events do
+    resources :bookings, only: [:create]
+    resources :users, only: :index
+  end
+  resources :users, only: [:show, :edit, :update, :destroy] do
+    resources :posts do
+      resources :comments, only: [:index, :new, :create, :edit, :update, :destroy]
+    end
+  end
+  resources :bookings, only: [:index, :destroy]
+
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
