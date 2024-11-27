3a4423036c6314a11d493faadb24a0b85edc8b58
Denis Sigal <182473309+densigal@users.noreply.github.com>
2024-11-27 11:26:23 +0900

    profile show and update are working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit]
   def dashboard
 
   end
 
   def show
-    @user = User.find(params[:id])
+
+  end
+
+  def edit
+
   end
 
   def update
+    @user = current_user
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :description, :photo)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
