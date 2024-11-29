2c7dff84d742f7461c812e0628b2908e06005f28
Stuart Yukinaga <175701806+YukinagaS@users.noreply.github.com>
2024-11-29 11:37:50 +0900

    added booking creation to event show

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,11 @@
 class BookingsController < ApplicationController
-
-
+  def create
+    @event = Event.find(params[:event_id])
+    @booking = Booking.new(event: @event, user: current_user)
+    if @booking.save
+      redirect_to event_path(@event)
+    else
+      render event_path(@event), status: :unprocessable_entity
+    end
+  end
 end
