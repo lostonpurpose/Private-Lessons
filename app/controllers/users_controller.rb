8f49a4c2107110b84de5dc623fda453857d3fa8c
Denis Sigal <182473309+densigal@users.noreply.github.com>
2024-12-05 16:33:44 +0900

    most attended working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -66,6 +66,5 @@ class UsersController < ApplicationController
       { user_id => most_attended.flatten.count(user_id)}
     end
     array.sort_by { |element| element.values[0] }.reverse[0..9]
-    raise
   end
 end
